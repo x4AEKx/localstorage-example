8d67853ebda8e68985d4235d035c296c64ac871f
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-05-02 18:10:21 +0300

    example using localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		const [value, setValue] = useState(0)
+
+		const incHandler = () => {
+				setValue(value + 1)
+		}
+
+		const setToLocalstorage = () => {
+				localStorage.setItem('counterValue', JSON.stringify(value))
+				localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+		}
+
+		const getFromLocalstorage = () => {
+				let valueAsString = localStorage.getItem('counterValue')
+				if(valueAsString) {
+						let newValue = JSON.parse(valueAsString)
+						setValue(newValue)
+				}
+		}
+
+		const clearLocalStorage = () => {
+				localStorage.clear()
+				setValue(0)
+		}
+
+		const removeItemFromLocalstorage = () => {
+				localStorage.removeItem('counterValue + 1')
+		}
+
+		return (
+				<div className="App">
+						<h1>{value}</h1>
+						<button onClick={incHandler}>inc</button>
+						<button onClick={setToLocalstorage}>setToLocalstorage</button>
+						<button onClick={getFromLocalstorage}>getFromLocalstorage</button>
+						<button onClick={clearLocalStorage}>clearLocalStorage</button>
+						<button onClick={removeItemFromLocalstorage}>removeItemFromLocalstorage</button>
+				</div>
+		);
 }
 
 export default App;
